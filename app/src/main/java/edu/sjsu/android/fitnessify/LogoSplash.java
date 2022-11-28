577235cda739f8de80f06a3cff828ae55e9f73f8
Abbhinav Reddie Nomulla <abbhinav.nomulla@sjsu.edu>
2022-11-28 03:12:04 -0800

    initialize

---
+++ app/src/main/java/edu/sjsu/android/fitnessify/LogoSplash.java
@@ -0,0 +1,29 @@
+package edu.sjsu.android.fitnessify;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogoSplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(LogoSplash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
---
+++ app/src/main/java/edu/sjsu/android/fitnessify/MainActivity.java
@@ -0,0 +1,69 @@
+package edu.sjsu.android.fitnessify;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import me.ibrahimsn.lib.OnItemSelectedListener;
+import me.ibrahimsn.lib.SmoothBottomBar;
+public class MainActivity extends AppCompatActivity {
+
+    private SmoothBottomBar bottomBar;
+    private static final int TIME_INTERVAL = 2000;
+    private long backPressed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //HomeFragment as default
+        replace(new Home());
+        //BottomNavBar selection
+        bottomBar = findViewById(R.id.bottomBar);
+        bottomBar.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public boolean onItemSelect(int i) {
+                switch (i){
+                    case 0:
+                        replace(new Home());
+                        break;
+
+                    case 1:
+                        replace(new Fitness());
+                        break;
+
+                    case 2:
+                        replace(new Profile());
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    //Replacing Fragments
+    private void replace(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame,fragment);
+        transaction.commit();
+    }
+
+    //Double click to Exit Start
+    @Override
+    public void onBackPressed(){
+        if(backPressed + TIME_INTERVAL > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finish();
+            return;
+        }
+        else{
+            Toast.makeText(getBaseContext(),"Press Back Again To Exit",Toast.LENGTH_SHORT).show();
+        }
+        backPressed = System.currentTimeMillis();
+    }
+}
